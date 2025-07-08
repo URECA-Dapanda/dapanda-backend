db6cdc8955eae9461b89a9b207af0c1f0c59236b
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-07-08 15:04:06 +0900

    [DP-27] feat: Member 엔티티에 빠져있던 속성 추가 #17

--- src/main/java/com/dapanda/member/entity/Member.java
+++ src/main/java/com/dapanda/member/entity/Member.java
@@ -1,26 +1,55 @@
 package com.dapanda.member.entity;
 
+import com.dapanda.auth.entity.OAuthProvider;
 import com.dapanda.common.entity.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"email", "provider"}))
 public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
     private String name;
 
+    private String password;
+
     private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OAuthProvider provider;
 
     private int buyingData;
 
     private int sellingData;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private MemberRole role;
 
     private int cash;
