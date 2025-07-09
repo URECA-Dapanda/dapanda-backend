e6ba084d820c84051d5eb2f0a6c5c5dd513ebc07
yereumi <lovelyyeji01@gmail.com>
2025-07-09 11:05:03 +0900

    [DP-34] chore: Trade 엔티티명 수정 #27

--- src/main/java/com/dapanda/trade/entity/Trade.java
+++ src/main/java/com/dapanda/trade/entity/Trade.java
@@ -1,14 +1,20 @@
-package com.dapanda.tradeHistory.entity;
+package com.dapanda.trade.entity;
 
 import com.dapanda.common.entity.CreatedAtEntity;
 import com.dapanda.member.entity.Member;
 import com.dapanda.product.entity.ProductType;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 
 @Entity
 @Getter
-public class TradeHistory extends CreatedAtEntity {
+public class Trade extends CreatedAtEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
