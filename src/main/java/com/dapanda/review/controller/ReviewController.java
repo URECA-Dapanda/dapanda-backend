4c72558cfed2c0bd519e83dcd79499cc6f6f9c1d
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-09 18:47:44 +0900

    [DP-33] feat: ReviewController 추가 #26

---
+++ src/main/java/com/dapanda/review/controller/ReviewController.java
@@ -0,0 +1,23 @@
+package com.dapanda.review.controller;
+
+import com.dapanda.common.exception.CommonResponse;
+import com.dapanda.review.dto.request.SaveReviewRequest;
+import com.dapanda.review.dto.response.SaveReviewResponse;
+import com.dapanda.review.service.ReviewService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api")
+@RestController
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping("/reviews")
+    public CommonResponse<SaveReviewResponse> saveReview(@RequestBody @Valid SaveReviewRequest request,@RequestParam Long memberId) {
+
+        return CommonResponse.success(reviewService.saveReview(request, memberId));
+    }
+}
