17e585ee3af2e0c4dce31d0849e315b2e2bba44e
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-09 18:49:43 +0900

    [DP-33] feat: 리뷰 등록 에러 코드 추가 #26

--- src/main/java/com/dapanda/common/exception/ResultCode.java
+++ src/main/java/com/dapanda/common/exception/ResultCode.java
@@ -15,8 +15,10 @@ public enum ResultCode {
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, 1001, "유효하지 않은 토큰입니다."),
 
     // 회원 2000번대
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, 2000, "회원을 찾을 수 없습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, 2000, "회원을 찾을 수 없습니다."),
 
+    // 리뷰 6000번대
+    SELF_REVIEW(HttpStatus.BAD_REQUEST, 6000, "자신에게 리뷰를 작성할 수 없습니다.")
     ;
 
     private final HttpStatus status;
