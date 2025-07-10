0e8eafb92d0ae99d047b437d64766704bb50434e
yereumi <lovelyyeji01@gmail.com>
2025-07-10 11:37:05 +0900

    [DP-34] feat: PaymentController CustomUserDetails에서 memberId 추출하는 로직 추가 #27

--- src/main/java/com/dapanda/payment/controller/PaymentController.java
+++ src/main/java/com/dapanda/payment/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package com.dapanda.payment.controller;
 
+import com.dapanda.auth.entity.CustomUserDetails;
 import com.dapanda.common.exception.CommonResponse;
 import com.dapanda.common.exception.ResultCode;
 import com.dapanda.payment.dto.request.SaveAmountRequest;
@@ -8,10 +9,10 @@ import com.dapanda.payment.dto.response.TossConfirmResponse;
 import com.dapanda.payment.service.PaymentService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,44 +20,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PaymentController {
 
-    private final PaymentService paymentService;
+	private final PaymentService paymentService;
 
-    @PostMapping("/payments/save-amount")
-    public CommonResponse<Void> saveAmount(HttpSession session,
-            @RequestBody SaveAmountRequest request) {
+	@PostMapping("/payments/save-amount")
+	public CommonResponse<Void> saveAmount(HttpSession session,
+			@RequestBody SaveAmountRequest request) {
 
-        session.setAttribute(request.orderId(), request.amount());
+		session.setAttribute(request.orderId(), request.amount());
 
-        return CommonResponse.success(null);
-    }
+		return CommonResponse.success(null);
+	}
 
-    @PostMapping("/payments/verify-amount")
-    public CommonResponse<Void> verifyAmount(HttpSession session,
-            @RequestBody SaveAmountRequest request) {
+	@PostMapping("/payments/verify-amount")
+	public CommonResponse<Void> verifyAmount(HttpSession session,
+			@RequestBody SaveAmountRequest request) {
 
-        String amount = String.valueOf(session.getAttribute(request.orderId()));
+		String amount = String.valueOf(session.getAttribute(request.orderId()));
 
-        if (amount == null) {
-            return new CommonResponse<>(ResultCode.INVALID_PAYMENT_AMOUNT);
-        }
-        if (!amount.equals(String.valueOf(request.amount()))) {
-            return new CommonResponse<>(ResultCode.PAYMENT_AMOUNT_MISMATCH);
-        }
+		if (amount == null) {
+			return new CommonResponse<>(ResultCode.INVALID_PAYMENT_AMOUNT);
+		}
+		if (!amount.equals(String.valueOf(request.amount()))) {
+			return new CommonResponse<>(ResultCode.PAYMENT_AMOUNT_MISMATCH);
+		}
 
-        return CommonResponse.success(null);
-    }
+		return CommonResponse.success(null);
+	}
 
-    // TODO: @AuthenticationPrincipal CustomUserDetails customUserDetails 적용
-    @PostMapping("/payments/confirm")
-    public CommonResponse<Void> confirm(
-            @RequestParam("memberId") Long memberId,
-            @RequestBody TossConfirmRequest request) {
+	@PostMapping("/payments/confirm")
+	public CommonResponse<Void> confirm(
+			@AuthenticationPrincipal CustomUserDetails customUserDetails,
+			@RequestBody TossConfirmRequest request) {
 
-//        Long memberId = customUserDetails.getId();
+		Long memberId = customUserDetails.getId();
 
-        TossConfirmResponse response = paymentService.confirmPayment(memberId, request);
-        paymentService.updateCash(memberId, response.totalAmount());
+		TossConfirmResponse response = paymentService.confirmPayment(memberId, request);
+		paymentService.updateCash(memberId, response.totalAmount());
 
-        return CommonResponse.success(null);
-    }
+		return CommonResponse.success(null);
+	}
 }
