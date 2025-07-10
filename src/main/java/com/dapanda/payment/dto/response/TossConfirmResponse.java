89235c8801af46759c4ca1762e74c64f0a152eae
yereumi <lovelyyeji01@gmail.com>
2025-07-10 09:18:45 +0900

    [DP-34] fix: TossConfirmResponse 필드 수정 #27

--- src/main/java/com/dapanda/payment/dto/response/TossConfirmResponse.java
+++ src/main/java/com/dapanda/payment/dto/response/TossConfirmResponse.java
@@ -3,8 +3,7 @@ package com.dapanda.payment.dto.response;
 public record TossConfirmResponse(
         String orderId,
         String paymentKey,
-        String totalAmount,
-        String method,
+        int totalAmount,
         String approvedAt
 ) {
 
