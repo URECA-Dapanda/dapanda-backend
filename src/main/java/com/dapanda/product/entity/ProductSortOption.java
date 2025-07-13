c7266cdb1aa7231830b592a42025e5961b4fff62
yereumi <lovelyyeji01@gmail.com>
2025-07-13 16:58:40 +0900

    [DP-46] feat: ProductSortOption 유효성 검증 로직 추가 #36

--- src/main/java/com/dapanda/product/entity/ProductSortOption.java
+++ src/main/java/com/dapanda/product/entity/ProductSortOption.java
@@ -1,21 +1,33 @@
 package com.dapanda.product.entity;
 
+import com.dapanda.common.exception.GlobalException;
+import com.dapanda.common.exception.ResultCode;
 import lombok.Getter;
 
 @Getter
 public enum ProductSortOption {
 
-	RECENT("RECENT"), // 최신순
-	PRICE_ASC("PRICE_ASC"), // 낮은 가격순
-	AMOUNT_ASC("AMOUNT_ASC"), // 데이터 용량 적은순
-	AMOUNT_DESC("AMOUNT_DESC"), // 데이터 용량 많은순
-	DISTANCE_ASC("DISTANCE_ASC"), // 거리 가까운순
-	AVERAGE_RATE_DESC("AVERAGE_RATE_DESC"); // 리뷰 평점 높은순
+	RECENT, // 최신순
+	PRICE_ASC, // 가격 낮은순
+	AMOUNT_ASC, // 데이터 용량 적은순
+	AMOUNT_DESC, // 데이터 용량 많은순
+	DISTANCE_ASC, // 거리 가까운순
+	AVERAGE_RATE_DESC, // 리뷰 평점 높은순
+	;
 
-	private final String code;
+	public static ProductSortOption from(String name) {
 
-	ProductSortOption(String code) {
+		if (name == null) {
 
-		this.code = code;
+			return RECENT; // 기본값: 최신순
+		}
+
+		try {
+
+			return ProductSortOption.valueOf(name);
+		} catch (IllegalArgumentException e) {
+
+			throw new GlobalException(ResultCode.INVALID_PRODUCT_SORT_OPTION);
+		}
 	}
 }
