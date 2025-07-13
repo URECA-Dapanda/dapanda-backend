e82096234bf224bfb893d7f05c0b129a7520d73e
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-13 23:08:46 +0900

    [DP-42] feat: Valid 메세지 추가 #38

--- src/main/java/com/dapanda/review/dto/request/DeleteReviewRequest.java
+++ src/main/java/com/dapanda/review/dto/request/DeleteReviewRequest.java
@@ -3,6 +3,6 @@ package com.dapanda.review.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record DeleteReviewRequest(
-		@NotNull
+		@NotNull(message = "삭제할 리뷰의 아이디는 필수입니다.")
 		Long reviewId) {
 }
