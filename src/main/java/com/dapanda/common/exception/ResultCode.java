e27388acf9170e15242a1920f6352a471f3244a6
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-13 20:52:16 +0900

    [DP-42] feat: 예외 메세지 수정 #38

--- src/main/java/com/dapanda/common/exception/ResultCode.java
+++ src/main/java/com/dapanda/common/exception/ResultCode.java
@@ -49,7 +49,7 @@ public enum ResultCode {
 
 	// 리뷰 6000번대
 	SELF_REVIEW(HttpStatus.BAD_REQUEST, 6000, "자신에게 리뷰를 작성할 수 없습니다."),
-	OTHER_REVIEW(HttpStatus.BAD_REQUEST, 6001, "자신의 리뷰만 삭제할 수 있습니다."),
+	OTHER_REVIEW(HttpStatus.BAD_REQUEST, 6001, "자신의 리뷰가 아닙니다."),
 
 	// 신고 7000번대
 
