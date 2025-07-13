758d35f8c40daee895e8418016f6cb34ff2f9a94
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-13 21:32:35 +0900

    [DP-42] test: 테스트 용 Fixture 수정 #38

--- src/test/java/com/dapanda/member/entity/MemberFixture.java
+++ src/test/java/com/dapanda/member/entity/MemberFixture.java
@@ -1,20 +1,49 @@
 package com.dapanda.member.entity;
 
 import com.dapanda.auth.entity.OAuthProvider;
+import org.springframework.test.util.ReflectionTestUtils;
 
 public class MemberFixture {
 
-    public static final Member MEMBER_REVIEWER = Member.ofOAuthMember(
-            "dummy1@email.com",
-            "dummy1Name",
-            OAuthProvider.KAKAO,
-            MemberRole.ROLE_MEMBER
-    );
+	public static final Member MEMBER_REVIEWER = Member.ofOAuthMember(
+			"dummy1@email.com",
+			"dummy1Name",
+			OAuthProvider.KAKAO,
+			MemberRole.ROLE_MEMBER
+	);
 
-    public static final Member MEMBER_REVIEWEE = Member.ofOAuthMember(
-            "dummy2@email.com",
-            "dummy2Name",
-            OAuthProvider.KAKAO,
-            MemberRole.ROLE_MEMBER
-    );
+	public static final Member MEMBER_REVIEWEE = Member.ofOAuthMember(
+			"dummy2@email.com",
+			"dummy2Name",
+			OAuthProvider.KAKAO,
+			MemberRole.ROLE_MEMBER
+	);
+
+	public static Member createMember1(Long memberId) {
+
+		Member member = Member.ofOAuthMember(
+				"test1@email.com",
+				"test1",
+				OAuthProvider.KAKAO,
+				MemberRole.ROLE_MEMBER
+		);
+
+		ReflectionTestUtils.setField(member, "id", memberId);
+
+		return member;
+	}
+
+    public static Member createMember2(Long memberId) {
+
+        Member member = Member.ofOAuthMember(
+                "test2@email.com",
+                "test2",
+                OAuthProvider.KAKAO,
+                MemberRole.ROLE_MEMBER
+        );
+
+        ReflectionTestUtils.setField(member, "id", memberId);
+
+        return member;
+    }
 }
