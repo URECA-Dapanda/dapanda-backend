e9c1efb7d301254c8d8a5316959d2ea76a58007b
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-14 10:09:15 +0900

    [DP-53] feat: 리뷰 수정 Controller 추가 #42

--- src/main/java/com/dapanda/review/controller/ReviewController.java
+++ src/main/java/com/dapanda/review/controller/ReviewController.java
@@ -4,7 +4,9 @@ import com.dapanda.auth.entity.CustomUserDetails;
 import com.dapanda.common.exception.CommonResponse;
 import com.dapanda.review.dto.request.DeleteReviewRequest;
 import com.dapanda.review.dto.request.SaveReviewRequest;
+import com.dapanda.review.dto.request.UpdateReviewRequest;
 import com.dapanda.review.dto.response.SaveReviewResponse;
+import com.dapanda.review.dto.response.UpdateReviewResponse;
 import com.dapanda.review.service.ReviewService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +26,14 @@ public class ReviewController {
 			@AuthenticationPrincipal CustomUserDetails userDetails) {
 
 		return CommonResponse.success(reviewService.saveReview(request, userDetails.getId()));
+	}
+
+	@PutMapping("/reviews")
+	public CommonResponse<UpdateReviewResponse> updateReview(
+			@RequestBody @Valid UpdateReviewRequest request,
+			@AuthenticationPrincipal CustomUserDetails userDetails){
+
+		return CommonResponse.success(reviewService.updateReview(request, userDetails.getId()));
 	}
 
 	@DeleteMapping("/reviews")
