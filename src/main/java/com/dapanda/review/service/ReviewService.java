9d70dbf12df96566f7e3eefef72d2769972315ba
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-14 13:34:55 +0900

    [DP-53] feat: 트랜잭션 추가 #42

--- src/main/java/com/dapanda/review/service/ReviewService.java
+++ src/main/java/com/dapanda/review/service/ReviewService.java
@@ -14,6 +14,7 @@ import com.dapanda.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -38,6 +39,7 @@ public class ReviewService {
 		return SaveReviewResponse.from(savedReview.getId());
 	}
 
+	@Transactional
 	public UpdateReviewResponse updateReview(UpdateReviewRequest request, Long memberId){
 
 		Review savedReview = reviewRepository.findById(request.reviewId())
