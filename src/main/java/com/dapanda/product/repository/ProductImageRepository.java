8370b6a9a8a7a779ae9e5edad8e5b79696adf505
yereumi <lovelyyeji01@gmail.com>
2025-07-14 21:07:01 +0900

    [DP-61] feat: ProductImageRepository 추가 #44

---
+++ src/main/java/com/dapanda/product/repository/ProductImageRepository.java
@@ -0,0 +1,10 @@
+package com.dapanda.product.repository;
+
+import com.dapanda.product.entity.ProductImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+
+}
