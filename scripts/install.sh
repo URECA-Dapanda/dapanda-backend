23e49b0a1dae45d650960b0dd2925317ef3641ff
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-07-14 11:20:14 +0900

    [DP-43] feat : ubuntu 내부에서 root 권한이 필요한 작업 sudo를 통해 진행하도록 설정 #34

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-
 echo "install phase"
-
 # 앱 프로세스 종료
 APP_NAME="app.jar"
 APP_DIR="/home/ubuntu/app"
 PID=$(pgrep -f $APP_NAME)
-
 if [ -n "$PID" ]; then
   echo "Stopping existing application (PID: $PID)"
-  sudo kill -9 "$PID"
-  rm -f "$APP_DIR/$APP_NAME"
+  kill -9 "$PID"
 else
   echo "No application to stop"
 fi
+echo "Fixing permissions on app directory"
+sudo mkdir -p /home/ubuntu/app
+sudo chown -R ubuntu:ubuntu /home/ubuntu/app
+sudo chmod -R 750 /home/ubuntu/app
+
+echo "Downloading CloudWatch Agent deb package"
+wget https://s3.ap-northeast-2.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb -O /tmp/amazon-cloudwatch-agent.deb
+
+echo "Installing CloudWatch Agent"
+sudo dpkg -i /tmp/amazon-cloudwatch-agent.deb || sudo apt-get -f install -y
 
 # 앱 디렉토리 생성
-mkdir -p "$APP_DIR"
+mkdir -p $APP_DIR
