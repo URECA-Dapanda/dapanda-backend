1ef374474b8d5687db2bb64fe6d95c1b146639d3
yereumi <lovelyyeji01@gmail.com>
2025-07-14 16:10:28 +0900

    [DP-46] test: 수정 사항 적용 #36

--- src/test/java/com/dapanda/product/controller/ProductControllerTest.java
+++ src/test/java/com/dapanda/product/controller/ProductControllerTest.java
@@ -5,6 +5,7 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,10 +36,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
@@ -87,15 +90,15 @@ class ProductControllerTest {
 			void getMobileDataByCursorTest() throws Exception {
 
 				// given
-				int size = 3;
+				int size = 2;
 				String productSortOption = "RECENT";
-				Integer dataAmount = 2;
+				Float dataAmount = 2.0F;
 
 				Member member = memberRepository.save(MemberFixture.MEMBER1);
 
-				MobileData mobileData1 = MobileDataFixture.createMobileData(2, 2, 500);
-				MobileData mobileData2 = MobileDataFixture.createMobileData(2, 2, 600);
-				MobileData mobileData3 = MobileDataFixture.createMobileData(3, 3, 700);
+				MobileData mobileData1 = MobileDataFixture.createMobileData(2.0F, 2.0F, 500);
+				MobileData mobileData2 = MobileDataFixture.createMobileData(2.0F, 2.0F, 600);
+				MobileData mobileData3 = MobileDataFixture.createMobileData(3.0F, 3.0F, 700);
 				mobileDataRepository.saveAll(List.of(mobileData1, mobileData2, mobileData3));
 
 				Product product1 = ProductFixture.createMobileDataProduct(3000,
@@ -106,14 +109,14 @@ class ProductControllerTest {
 						mobileData3.getId(), member);
 				productRepository.saveAll(List.of(product1, product2, product3));
 
-				MobileDataCursorRequest request = new MobileDataCursorRequest(null, size,
+				MobileDataCursorRequest request = new MobileDataCursorRequest(3L, size,
 						productSortOption, dataAmount);
 
 				// when & then
 				mockMvc.perform(
-								org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get(
+								MockMvcRequestBuilders.post(
 												"/api/products/mobile-data")
-										.contentType(org.springframework.http.MediaType.APPLICATION_JSON)
+										.contentType(MediaType.APPLICATION_JSON)
 										.content(objectMapper.writeValueAsString(request))
 						)
 						.andExpect(status().isOk())
@@ -143,8 +146,6 @@ class ProductControllerTest {
 												"등록한 회원 이름"),
 										fieldWithPath("data.data[].remainAmount").description(
 												"데이터 잔여량"),
-										fieldWithPath("data.data[].unit").description(
-												"데이터 단위(MB, GB)"),
 										fieldWithPath("data.data[].pricePer100MB").description(
 												"100MB당 가격"),
 										fieldWithPath("data.data[].splitType").description(
@@ -153,7 +154,7 @@ class ProductControllerTest {
 												"다음 커서 아이디"),
 										fieldWithPath("data.pageInfo.hasNext").description(
 												"다음 페이지 존재 여부"),
-										fieldWithPath("data.pageInfo.size").description("데이터 크기")
+										fieldWithPath("data.pageInfo.size").description("페이지 크기")
 								))
 						);
 			}
@@ -168,18 +169,16 @@ class ProductControllerTest {
 
 					// given
 					List<MobileDataCursorRequest> invalidRequests = List.of(
-							new MobileDataCursorRequest(null, 0, "RECENT", 2),
-							new MobileDataCursorRequest(null, null, "RECENT", 2)
+							new MobileDataCursorRequest(3L, 0, "RECENT", 2.0F),
+							new MobileDataCursorRequest(3L, null, "RECENT", 2.0F)
 					);
 
 					// when & then
 					for (MobileDataCursorRequest request : invalidRequests) {
-						mockMvc.perform(
-										org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get(
-														"/api/products/mobile-data")
-												.contentType(
-														org.springframework.http.MediaType.APPLICATION_JSON)
-												.content(objectMapper.writeValueAsString(request))
+						mockMvc.perform(MockMvcRequestBuilders.post(
+												"/api/products/mobile-data")
+										.contentType(MediaType.APPLICATION_JSON)
+										.content(objectMapper.writeValueAsString(request))
 								)
 								.andExpect(status().isBadRequest())
 								.andExpect(jsonPath("$.message").value(
@@ -195,16 +194,14 @@ class ProductControllerTest {
 				void throwExceptionWhenProductSortOptionIsInvalid() throws Exception {
 
 					// given
-					MobileDataCursorRequest request = new MobileDataCursorRequest(null, 2,
-							"RECENT123", 2);
+					MobileDataCursorRequest request = new MobileDataCursorRequest(3L, 2,
+							"RECENT123", 2.0F);
 
 					// when & then
-					mockMvc.perform(
-									org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get(
-													"/api/products/mobile-data")
-											.contentType(
-													org.springframework.http.MediaType.APPLICATION_JSON)
-											.content(objectMapper.writeValueAsString(request))
+					mockMvc.perform(MockMvcRequestBuilders.post(
+											"/api/products/mobile-data")
+									.contentType(MediaType.APPLICATION_JSON)
+									.content(objectMapper.writeValueAsString(request))
 							)
 							.andExpect(status().isBadRequest())
 							.andExpect(jsonPath("$.message").value(
@@ -230,56 +227,54 @@ class ProductControllerTest {
 			void getWifiByCursorTest() throws Exception {
 
 				// given
-				int size = 3;
-				String productSortOption = "RECENT";
+				Long cursorId = 1L;
+				int size = 2;
+				String productSortOption = "DISTANCE_ASC";
 				boolean isOpen = false;
-				double latitude = 30;
-				double longitude = 127;
+				double latitude = 30.0;
+				double longitude = 127.0;
 
 				Member member = memberRepository.save(MemberFixture.MEMBER1);
 
-				Wifi wifi1 = WifiFixture.createWifi1("제목1", "내용1", 30, 126,
+				Wifi wifi1 = WifiFixture.createWifi1("제목1", "내용1", 30.0, 126.0,
 						LocalDateTime.of(2025, 7, 14, 10, 0),
 						LocalDateTime.of(2025, 7, 14, 18, 0));
-				Wifi wifi2 = WifiFixture.createWifi1("제목2", "내용2", 30, 126,
+				Wifi wifi2 = WifiFixture.createWifi1("제목2", "내용2", 30.0, 126.0,
 						LocalDateTime.of(2025, 7, 14, 10, 0),
 						LocalDateTime.of(2025, 7, 14, 18, 0));
-				Wifi wifi3 = WifiFixture.createWifi1("제목2", "내용3", 30, 126,
+				Wifi wifi3 = WifiFixture.createWifi1("제목3", "내용3", 30.0, 126.0,
 						LocalDateTime.of(2025, 7, 14, 10, 0),
 						LocalDateTime.of(2025, 7, 14, 18, 0));
 				wifiRepository.saveAll(List.of(wifi1, wifi2, wifi3));
 
-				Product product1 = ProductFixture.createMobileDataProduct(3000, wifi1.getId(),
-						member);
-				Product product2 = ProductFixture.createMobileDataProduct(4000, wifi2.getId(),
-						member);
-				Product product3 = ProductFixture.createMobileDataProduct(5000, wifi3.getId(),
-						member);
+				Product product1 = ProductFixture.createWifiProduct(3000, wifi1.getId(), member);
+				Product product2 = ProductFixture.createWifiProduct(4000, wifi2.getId(), member);
+				Product product3 = ProductFixture.createWifiProduct(5000, wifi3.getId(), member);
 				productRepository.saveAll(List.of(product1, product2, product3));
 
-				WifiCursorRequest request = new WifiCursorRequest(null, size,
+				WifiCursorRequest request = new WifiCursorRequest(cursorId, size,
 						productSortOption, isOpen, latitude, longitude);
 
 				// when & then
-				mockMvc.perform(
-								org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get(
-												"/api/products/wifi")
-										.contentType(org.springframework.http.MediaType.APPLICATION_JSON)
-										.content(objectMapper.writeValueAsString(request))
+				mockMvc.perform(MockMvcRequestBuilders.post(
+										"/api/products/wifi")
+								.contentType(MediaType.APPLICATION_JSON)
+								.content(objectMapper.writeValueAsString(request))
 						)
 						.andExpect(status().isOk())
 						.andExpect(jsonPath("$.code").value(ResultCode.SUCCESS.getCode()))
 						.andExpect(jsonPath("$.message").value(ResultCode.SUCCESS.getMessage()))
 						.andExpect(jsonPath("$.data").exists())
+						.andDo(print())
 						.andDo(document("product/get-products-wifi",
 								requestFields(
 										fieldWithPath("cursorId").description("마지막 커서 아이디 (필수 X)"),
 										fieldWithPath("size").description("페이지 사이즈 (필수, 1 이상 정수)"),
 										fieldWithPath("productSortOption").description(
-												"정렬 조건 (필수 X, 기본값: 최신순) - RECENT(최신순), PRICE_ASC(가격 낮은순), AMOUNT_ASC(데이터 용량 적은순), AMOUNT_DESC(데이터 용량 많은순"),
+												"정렬 조건 (필수 X) - PRICE_ASC(가격 낮은순), AVERAGE_RATE_DESC(평점 높은순)"),
 										fieldWithPath("open").description("영업중 여부(필수 X)"),
-										fieldWithPath("latitude").description("위도"),
-										fieldWithPath("longitude").description("경도")
+										fieldWithPath("latitude").description("사용자의 위도"),
+										fieldWithPath("longitude").description("사용자의 경도")
 								),
 								responseFields(
 										fieldWithPath("code").description("상태 코드"),
@@ -291,7 +286,7 @@ class ProductControllerTest {
 										fieldWithPath("data.data[].price").description(
 												"상품 가격"),
 										fieldWithPath("data.data[].itemId").description(
-												"모바일 데이터 아이디"),
+												"와이파이 아이디"),
 										fieldWithPath("data.data[].memberName").description(
 												"등록한 회원 이름"),
 										fieldWithPath("data.data[].title").description("게시물 제목"),
@@ -307,7 +302,7 @@ class ProductControllerTest {
 												"다음 커서 아이디"),
 										fieldWithPath("data.pageInfo.hasNext").description(
 												"다음 페이지 존재 여부"),
-										fieldWithPath("data.pageInfo.size").description("데이터 크기")
+										fieldWithPath("data.pageInfo.size").description("페이지 크기")
 								))
 						);
 			}
@@ -323,18 +318,16 @@ class ProductControllerTest {
 
 				// given
 				List<WifiCursorRequest> invalidRequests = List.of(
-						new WifiCursorRequest(null, 0, "RECENT", true, 30.0, 126.0),
-						new WifiCursorRequest(null, null, "RECENT", true, 30.0, 126.0)
+						new WifiCursorRequest(3L, 0, "RECENT", true, 30.0, 126.0),
+						new WifiCursorRequest(4L, null, "RECENT", true, 30.0, 126.0)
 				);
 
 				// when & then
 				for (WifiCursorRequest request : invalidRequests) {
-					mockMvc.perform(
-									org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get(
-													"/api/products/wifi")
-											.contentType(
-													org.springframework.http.MediaType.APPLICATION_JSON)
-											.content(objectMapper.writeValueAsString(request))
+					mockMvc.perform(MockMvcRequestBuilders.post(
+											"/api/products/wifi")
+									.contentType(MediaType.APPLICATION_JSON)
+									.content(objectMapper.writeValueAsString(request))
 							)
 							.andExpect(status().isBadRequest())
 							.andExpect(jsonPath("$.message").value(
@@ -350,16 +343,14 @@ class ProductControllerTest {
 			void throwExceptionWhenProductSortOptionIsInvalid() throws Exception {
 
 				// given
-				WifiCursorRequest request = new WifiCursorRequest(null, 2,
+				WifiCursorRequest request = new WifiCursorRequest(3L, 2,
 						"RECENT123", true, null, null);
 
 				// when & then
-				mockMvc.perform(
-								org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get(
-												"/api/products/wifi")
-										.contentType(
-												org.springframework.http.MediaType.APPLICATION_JSON)
-										.content(objectMapper.writeValueAsString(request))
+				mockMvc.perform(MockMvcRequestBuilders.post(
+										"/api/products/wifi")
+								.contentType(MediaType.APPLICATION_JSON)
+								.content(objectMapper.writeValueAsString(request))
 						)
 						.andExpect(status().isBadRequest())
 						.andExpect(jsonPath("$.message").value(
