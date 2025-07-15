871afa8b2010ccd9cff55cf2ddc450f78c14bf2b
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-15 14:05:51 +0900

    [DP-35] feat: 거래내역 생성자 추가 #30

--- src/main/java/com/dapanda/trade/entity/Trade.java
+++ src/main/java/com/dapanda/trade/entity/Trade.java
@@ -3,10 +3,13 @@ package com.dapanda.trade.entity;
 import com.dapanda.common.entity.CreatedAtEntity;
 import com.dapanda.member.entity.Member;
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
 
 @Entity
 @Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Trade extends CreatedAtEntity {
 
 	@Id
@@ -24,4 +27,15 @@ public class Trade extends CreatedAtEntity {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
+
+	public static Trade of(Float dataAmount, Integer timeAmount, int tradingPrice, Long productId, Member member) {
+
+		return Trade.builder()
+				.dataAmount(dataAmount)
+				.timeAmount(timeAmount)
+				.tradingPrice(tradingPrice)
+				.productId(productId)
+				.member(member)
+				.build();
+	}
 }
