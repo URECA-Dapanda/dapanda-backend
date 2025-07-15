e13e658ac1ef2e22f3c56a0f73568b891810bb23
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-15 18:09:14 +0900

    [DP-35] feat: 판매자 리뷰 조회 삭제 #30

--- src/main/java/com/dapanda/review/dto/response/ReadSellerReviewResponse.java
+++ src/main/java/com/dapanda/review/dto/response/ReadSellerReviewResponse.java
@@ -1,66 +1,65 @@
-package com.dapanda.review.dto.response;
-
-import com.dapanda.product.entity.ItemType;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
-import java.time.LocalDateTime;
-
-@Getter
-@Builder
-@AllArgsConstructor
-public class ReadSellerReviewResponse {
-
-	//Review
-	private Long reviewId;
-	private Long reviewerId;
-	private Float rating;
-	private String comment;
-	private LocalDateTime createdAt;
-	private LocalDateTime updatedAt;
-	//Member
-	private String reviewerName;
-	//Trade
-	private Long tradeId;
-	@JsonInclude(JsonInclude.Include.NON_EMPTY)
-	private Float dataAmount;
-	@JsonInclude(JsonInclude.Include.NON_EMPTY)
-	private Integer timeAmount;
-	//Product
-	private Long productId;
-	private ItemType itemType;
-
-
-	public static ReadSellerReviewResponse of(
-			Long reviewId,
-			Long reviewerId,
-			Float rating,
-			String comment,
-			LocalDateTime createdAt,
-			LocalDateTime updatedAt,
-			String reviewerName,
-			Long tradeId,
-			Float dataAmount,
-			Integer timeAmount,
-			Long productId,
-			ItemType itemType
-	) {
-
-		return ReadSellerReviewResponse.builder()
-				.reviewId(reviewId)
-				.reviewerId(reviewerId)
-				.rating(rating)
-				.comment(comment)
-				.createdAt(createdAt)
-				.updatedAt(updatedAt)
-				.reviewerName(reviewerName)
-				.tradeId(tradeId)
-				.dataAmount(dataAmount)
-				.timeAmount(timeAmount)
-				.productId(productId)
-				.itemType(itemType)
-				.build();
-	}
-}
+//package com.dapanda.review.dto.response;
+//
+//import com.dapanda.product.entity.ItemType;
+//import com.fasterxml.jackson.annotation.JsonInclude;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//
+//import java.time.LocalDateTime;
+//
+//@Getter
+//@Builder
+//@AllArgsConstructor
+//public class ReadSellerReviewResponse {
+//
+//	//Review
+//	private Long reviewId;
+//	private Float rating;
+//	private String comment;
+//	private LocalDateTime createdAt;
+//	private LocalDateTime updatedAt;
+//	//Member
+//	private Long reviewerId;
+//	private String reviewerName;
+//	//Trade
+//	private Long tradeId;
+//	@JsonInclude(JsonInclude.Include.NON_EMPTY)
+//	private Float dataAmount;
+//	@JsonInclude(JsonInclude.Include.NON_EMPTY)
+//	private Integer timeAmount;
+//	//Product
+//	private Long productId;
+//	private ItemType itemType;
+//
+//	public static ReadSellerReviewResponse of(
+//			Long reviewId,
+//			Float rating,
+//			String comment,
+//			LocalDateTime createdAt,
+//			LocalDateTime updatedAt,
+//			Long reviewerId,
+//			String reviewerName,
+//			Long tradeId,
+//			Float dataAmount,
+//			Integer timeAmount,
+//			Long productId,
+//			ItemType itemType
+//	) {
+//
+//		return ReadSellerReviewResponse.builder()
+//				.reviewId(reviewId)
+//				.rating(rating)
+//				.comment(comment)
+//				.createdAt(createdAt)
+//				.updatedAt(updatedAt)
+//				.reviewerId(reviewerId)
+//				.reviewerName(reviewerName)
+//				.tradeId(tradeId)
+//				.dataAmount(dataAmount)
+//				.timeAmount(timeAmount)
+//				.productId(productId)
+//				.itemType(itemType)
+//				.build();
+//	}
+//}
