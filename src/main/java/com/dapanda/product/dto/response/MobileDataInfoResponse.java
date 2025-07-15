50708fc4362597c158ce4c6d9f30ce35eda7d65e
yereumi <lovelyyeji01@gmail.com>
2025-07-15 11:37:42 +0900

    [DP-51] fix: 불필요한 of 메서드와 어노테이션 삭제 #41

--- src/main/java/com/dapanda/product/dto/response/MobileDataInfoResponse.java
+++ src/main/java/com/dapanda/product/dto/response/MobileDataInfoResponse.java
@@ -2,15 +2,10 @@ package com.dapanda.product.dto.response;
 
 import com.dapanda.member.entity.Member;
 import java.time.LocalDateTime;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@Builder(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class MobileDataInfoResponse {
 
@@ -23,21 +18,4 @@ public class MobileDataInfoResponse {
 	private double averageRate;
 	private int reviewCount;
 	private LocalDateTime updatedAt;
-
-	public static MobileDataInfoResponse of(Long productId, Long itemId, int price, Member member,
-			float remainAmount, int pricePer100MB, double averageRate, int reviewCount,
-			LocalDateTime updatedAt) {
-
-		return MobileDataInfoResponse.builder()
-				.productId(productId)
-				.itemId(itemId)
-				.price(price)
-				.member(member)
-				.remainAmount(remainAmount)
-				.pricePer100MB(pricePer100MB)
-				.averageRate(averageRate)
-				.reviewCount(reviewCount)
-				.updatedAt(updatedAt)
-				.build();
-	}
 }
