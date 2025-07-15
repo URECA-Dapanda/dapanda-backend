95b534bf81d3beb7b148a55215d524f9b55cb36d
yereumi <lovelyyeji01@gmail.com>
2025-07-15 13:56:24 +0900

    [DP-51] fix: WifiFixture 메서드명 수정 및 사용하지 않는 메서드 삭제 #41

--- src/test/java/com/dapanda/product/entity/WifiFixture.java
+++ src/test/java/com/dapanda/product/entity/WifiFixture.java
@@ -2,11 +2,10 @@ package com.dapanda.product.entity;
 
 import com.dapanda.product.dto.WifiSummary;
 import java.time.LocalDateTime;
-import org.springframework.test.util.ReflectionTestUtils;
 
 public class WifiFixture {
 
-	public static Wifi createWifi1(String title, String content, double latitude, double longitude,
+	public static Wifi createWifi(String title, String content, double latitude, double longitude,
 			LocalDateTime startTime, LocalDateTime endTime) {
 
 		return Wifi.of(
@@ -19,25 +18,8 @@ public class WifiFixture {
 		);
 	}
 
-	public static Wifi createWifi2(Long wifiId) {
-
-		Wifi wifi = Wifi.of(
-				"와이파이 팝니다",
-				"선릉역 2번출구 앞 카페 와이파이",
-				30.1,
-				126.3,
-				LocalDateTime.of(2025, 7, 13, 10, 0),
-				LocalDateTime.of(2025, 7, 13, 22, 0)
-		);
-
-		ReflectionTestUtils.setField(wifi, "id", wifiId);
-
-		return wifi;
-	}
-
 	public static WifiSummary createWifiSummary(Long id, int price, Long itemId, String memberName,
-			String title,
-			double latitude, double longitude, double averageRate,
+			String title, double latitude, double longitude, double averageRate,
 			double distanceKm) {
 
 		return new WifiSummary(
