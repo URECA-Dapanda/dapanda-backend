dedf8cc7695d83e867949091d15aae18ac543f39
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-16 21:34:04 +0900

    [DP-19] fix: Bean Validation 수정 #9

--- src/main/java/com/dapanda/report/dto/request/CreateReportRequest.java
+++ src/main/java/com/dapanda/report/dto/request/CreateReportRequest.java
@@ -1,14 +1,14 @@
 package com.dapanda.report.dto.request;
 
 import com.dapanda.report.entity.ReportTargetCategory;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record CreateReportRequest(
 
 		@NotBlank(message = "신고 사유는 필수입니다.")
-		@Max(value = 100, message = "신고 사유는 최대 100자 입니다.")
+		@Size(max = 100, message = "신고 사유는 최대 100자 입니다.")
 		String reason,
 		@NotNull(message = "신고 대상 유형은 필수입니다.")
 		ReportTargetCategory targetCategory) {
