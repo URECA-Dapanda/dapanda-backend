7d6e08c77bd0d2eb2ea0e2a47d989f1aecc8cd1c
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-16 19:11:00 +0900

    [DP-19] feat: 신고 Service 추가 #9

--- src/main/java/com/dapanda/report/service/ReportService.java
+++ src/main/java/com/dapanda/report/service/ReportService.java
@@ -6,9 +6,11 @@ import com.dapanda.member.entity.Member;
 import com.dapanda.member.repository.MemberRepository;
 import com.dapanda.report.dto.CreateReportRequest;
 import com.dapanda.report.entity.Report;
+import com.dapanda.report.entity.ReportTargetCategory;
 import com.dapanda.report.repository.ReportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +19,7 @@ public class ReportService {
 	private final ReportRepository reportRepository;
 	private final MemberRepository memberRepository;
 
+	@Transactional
 	public void createReport(Long targetId, Long memberId, CreateReportRequest request) {
 
 		Member reporter = memberRepository.findById(memberId)
@@ -25,6 +28,27 @@ public class ReportService {
 		Report report = Report.of(request.reason(), targetId, request.targetCategory(), reporter);
 
 		reportRepository.save(report);
+
+		Long reportTargetMemberId = findReportTargetMemberId(targetId, request.targetCategory());
+
+		Member reportedMember = memberRepository.findByIdWithLock(reportTargetMemberId)
+				.orElseThrow(() -> new GlobalException(ResultCode.MEMBER_NOT_FOUND));
+
+		reportedMember.increaseReportedCount();
 	}
 
+	private Long findReportTargetMemberId(Long targetId, ReportTargetCategory category) {
+
+		return switch (category) {
+
+			case MEMBER -> memberRepository.findById(targetId)
+					.orElseThrow(() -> new GlobalException(ResultCode.MEMBER_NOT_FOUND)).getId();
+
+			case PRODUCT -> memberRepository.findMemberIdByProductId(targetId)
+					.orElseThrow(() -> new GlobalException(ResultCode.PRODUCT_NOT_FOUND));
+
+			case REVIEW -> memberRepository.findMemberIdByReviewId(targetId)
+					.orElseThrow(() -> new GlobalException(ResultCode.REVIEW_NOT_FOUND));
+		};
+	}
 }
