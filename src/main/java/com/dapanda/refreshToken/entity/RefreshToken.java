7ace3eae721f51deefbb2e17803e3890656a4deb
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-07-16 14:18:33 +0900

    [DP-62] feat : Jar 파일로 만들 때 api-docs.html을 포함하는지 로그로 확인 #46

--- src/main/java/com/dapanda/refreshToken/entity/RefreshToken.java
+++ src/main/java/com/dapanda/refreshToken/entity/RefreshToken.java
@@ -2,8 +2,23 @@ package com.dapanda.refreshToken.entity;
 
 import com.dapanda.common.entity.CreatedAtEntity;
 import com.dapanda.member.entity.Member;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -24,7 +39,7 @@ public class RefreshToken extends CreatedAtEntity {
 	@Builder.Default
 	private TokenState state = TokenState.VALID;
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
 	@JoinColumn(name = "member_id")
 	private Member member;
 
