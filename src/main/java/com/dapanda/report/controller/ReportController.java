b273ee7e0e618620b99fe2c2b659cc4492cddd9f
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-16 20:26:26 +0900

    [DP-19] feat: @Valid 추가 #9

--- src/main/java/com/dapanda/report/controller/ReportController.java
+++ src/main/java/com/dapanda/report/controller/ReportController.java
@@ -5,6 +5,7 @@ import com.dapanda.common.exception.CommonResponse;
 import com.dapanda.report.dto.request.CreateReportRequest;
 import com.dapanda.report.dto.response.CreateReportResponse;
 import com.dapanda.report.service.ReportService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,7 @@ public class ReportController {
 	public CommonResponse<CreateReportResponse> createReport(
 			@PathVariable Long targetId,
 			@AuthenticationPrincipal CustomUserDetails userDetails,
-			@RequestBody CreateReportRequest request) {
+			@RequestBody @Valid CreateReportRequest request) {
 
 		return CommonResponse.success(reportService.createReport(targetId, userDetails.getId(), request));
 	}
