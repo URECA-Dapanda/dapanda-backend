a6a70030416ffb3ac013a652d39231e54f902973
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-07-17 02:40:52 +0900

    [DP-62] feat : TimeZone 세팅 #46

--- src/main/java/com/dapanda/Application.java
+++ src/main/java/com/dapanda/Application.java
@@ -1,5 +1,7 @@
 package com.dapanda;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +12,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
