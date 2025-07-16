28150f35819cde84ee0d6dad356c1a99e2338b8c
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-16 19:10:47 +0900

    [DP-19] feat: 신고 카운팅 메서드 추가 #9

--- src/main/java/com/dapanda/member/entity/Member.java
+++ src/main/java/com/dapanda/member/entity/Member.java
@@ -2,20 +2,8 @@ package com.dapanda.member.entity;
 
 import com.dapanda.auth.entity.OAuthProvider;
 import com.dapanda.common.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
@@ -82,6 +70,11 @@ public class Member extends BaseEntity {
 				.provider(provider)
 				.role(role)
 				.build();
+	}
+
+	public void increaseReportedCount() {
+
+		this.reportedCount++;
 	}
 
 	public void addCash(int amount) {
