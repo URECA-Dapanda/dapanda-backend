0c9c648b142c3ee900a4c191b27f0606a545e7b7
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-16 16:31:08 +0900

    [DP-75] refactor: 리뷰 Controller URI 리팩터링 #55

--- src/main/java/com/dapanda/review/dto/request/DeleteReviewRequest.java
+++
@@ -1,9 +0,0 @@
-package com.dapanda.review.dto.request;
-
-import jakarta.validation.constraints.NotNull;
-
-public record DeleteReviewRequest(
-
-		@NotNull(message = "삭제할 리뷰의 아이디는 필수입니다.")
-		Long reviewId) {
-}
