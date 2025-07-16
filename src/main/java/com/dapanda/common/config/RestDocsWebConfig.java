9b393bc3d3547b7d173dd0301725641907eeae68
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-07-16 22:46:50 +0900

    [DP-62] feat : api-dos.html의 도메인을 재구성 #46

--- src/main/java/com/dapanda/common/config/RestDocsWebConfig.java
+++ src/main/java/com/dapanda/common/config/RestDocsWebConfig.java
@@ -1,17 +1,14 @@
 package com.dapanda.common.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class RestDocsWebConfig implements WebMvcConfigurer {
 
 	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-		registry.addResourceHandler("/docs/**")
-				.addResourceLocations("classpath:/static/docs/")
-				.setCachePeriod(3600);
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/docs/", "/docs/api-docs.html");
 	}
 }
