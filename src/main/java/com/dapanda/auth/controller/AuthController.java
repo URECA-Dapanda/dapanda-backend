bf4bdbab545c4d3d678afcde31ea0ffe6ee977bc
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-07-18 01:07:40 +0900

    [DP-81] feat : Access 토큰이 만료되어서 재발급 받는 로직을 JwtAuthenticationFilter 내부에서 수행하도록 변경  #61

--- src/main/java/com/dapanda/auth/controller/AuthController.java
+++ src/main/java/com/dapanda/auth/controller/AuthController.java
@@ -4,16 +4,13 @@ import com.dapanda.auth.dto.request.LoginRequest;
 import com.dapanda.auth.dto.request.SignupRequest;
 import com.dapanda.auth.dto.response.LoginResponse;
 import com.dapanda.auth.dto.response.SignupResponse;
-import com.dapanda.auth.dto.response.TokenResponse;
 import com.dapanda.auth.entity.OAuthProvider;
-import com.dapanda.auth.service.AuthService;
 import com.dapanda.common.exception.CommonResponse;
 import com.dapanda.common.exception.GlobalException;
 import com.dapanda.common.exception.ResultCode;
 import com.dapanda.jwt.JwtTokenProvider;
 import com.dapanda.member.entity.Member;
 import com.dapanda.member.service.MemberService;
-import com.dapanda.refreshToken.entity.TokenState;
 import com.dapanda.refreshToken.service.RefreshTokenService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -34,42 +31,6 @@ public class AuthController {
 	private final JwtTokenProvider jwtTokenProvider;
 	private final RefreshTokenService refreshTokenService;
 	private final MemberService memberService;
-	private final AuthService authService;
-
-	@PostMapping("/auth/reissue")
-	public CommonResponse<TokenResponse> reissueAccessToken(
-			HttpServletRequest request) {
-
-		String refreshToken = request.getHeader("Refresh-Token");
-		log.info("요청 {}", request.getRequestURI());
-		log.info("리프레시 토큰 : {}", refreshToken);
-
-		// 유효성 검사
-		if (refreshToken == null) {
-			throw new GlobalException(ResultCode.MISSING_TOKEN);
-		}
-		if (!jwtTokenProvider.validateToken(refreshToken)) {
-			throw new GlobalException(ResultCode.INVALID_TOKEN);
-		}
-
-		String email = jwtTokenProvider.getUserEmailFromToken(refreshToken);
-		OAuthProvider provider = jwtTokenProvider.getProviderFromToken(refreshToken);
-		Member member = memberService.findUserByEmailAndProvider(email, provider);
-
-		var savedToken = refreshTokenService.findByUser(member)
-				.orElseThrow(() -> new GlobalException(ResultCode.MISSING_TOKEN));
-
-		if (!savedToken.getToken().equals(refreshToken)) {
-			throw new GlobalException(ResultCode.TOKEN_REISSUE_FAILED);
-		}
-		if (savedToken.getState() != TokenState.VALID) {
-			throw new GlobalException(ResultCode.INVALID_TOKEN);
-		}
-
-		TokenResponse tokenResponse = authService.reissueAccessToken(refreshToken);
-
-		return CommonResponse.success(tokenResponse);
-	}
 
 	@PostMapping("/auth/signup")
 	public CommonResponse<SignupResponse> signup(
