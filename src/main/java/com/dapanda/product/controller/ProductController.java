5c86234ef5fb446ca441e6ff5ee5d45b03efd5b9
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-18 15:47:40 +0900

    [DP-89] feat: 회원의 판매 상태별 조회 Controller 추가 #72

--- src/main/java/com/dapanda/product/controller/ProductController.java
+++ src/main/java/com/dapanda/product/controller/ProductController.java
@@ -5,26 +5,16 @@ import com.dapanda.common.dto.response.CursorPageResponse;
 import com.dapanda.common.exception.CommonResponse;
 import com.dapanda.product.dto.MobileDataSummary;
 import com.dapanda.product.dto.WifiSummary;
-import com.dapanda.product.dto.request.MobileDataCursorRequest;
-import com.dapanda.product.dto.request.UpdateMobileDataRequest;
-import com.dapanda.product.dto.request.UpdateWifiRequest;
-import com.dapanda.product.dto.request.WifiCursorRequest;
-import com.dapanda.product.dto.response.MobileDataInfoResponse;
-import com.dapanda.product.dto.response.UpdateMobileDataResponse;
-import com.dapanda.product.dto.response.UpdateWifiResponse;
-import com.dapanda.product.dto.response.WifiInfoResponse;
+import com.dapanda.product.dto.request.*;
+import com.dapanda.product.dto.response.*;
+import com.dapanda.product.entity.ProductState;
 import com.dapanda.product.service.ProductService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -32,6 +22,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
 	private final ProductService productService;
+
+	@GetMapping("/members/{memberId}/selling-products")
+	public CommonResponse<CursorPageResponse<ReadSellingProductResponse>> readSellingProductHistory(
+
+			@PathVariable Long memberId,
+			@RequestParam ProductState state,
+			@RequestParam(required = false) Long cursorId,
+			@RequestParam(defaultValue = "2") @Min(1) @Max(100) Integer size,
+			@RequestParam(defaultValue = "RECENT") String reviewSortOption) {
+
+		ReadSellingProductRequest request = new ReadSellingProductRequest(cursorId, size, reviewSortOption, memberId, state);
+
+		return CommonResponse.success(productService.readSellingProduct(request));
+	}
 
 	@PostMapping("/products/mobile-data")
 	public CommonResponse<CursorPageResponse<MobileDataSummary>> getMobileDataByCursor(
