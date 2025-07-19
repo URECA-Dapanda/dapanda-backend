1c6195e996e77d04b55b3c15432deca25c5d0113
yereumi <lovelyyeji01@gmail.com>
2025-07-19 21:54:18 +0900

    [DP-76] feat: 상품 조회 시 비관적 Lock 쿼리 추가 #58

--- src/main/java/com/dapanda/product/repository/ProductRepository.java
+++ src/main/java/com/dapanda/product/repository/ProductRepository.java
@@ -1,10 +1,19 @@
 package com.dapanda.product.repository;
 
 import com.dapanda.product.entity.Product;
+import jakarta.persistence.LockModeType;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductCustomRepository {
 
+	// Lock 걸고 상품 조회
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query("SELECT p FROM Product p WHERE p.id = :id")
+	Optional<Product> findByIdForUpdate(@Param("id") Long id);
 }
