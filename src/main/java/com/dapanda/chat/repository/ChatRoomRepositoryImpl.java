2fd714f34d94ff955295f292bfa32f4c34863eb2
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-20 00:59:28 +0900

    [DP-87] feat: 기존 채팅방 조회 쿼리 추가 #68

---
+++ src/main/java/com/dapanda/chat/repository/ChatRoomRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.dapanda.chat.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+import static com.dapanda.chat.entity.QChatParticipant.chatParticipant;
+import static com.dapanda.chat.entity.QChatRoom.chatRoom;
+
+@RequiredArgsConstructor
+public class ChatRoomRepositoryImpl implements ChatRoomRepositoryCustom {
+
+	private final JPAQueryFactory jpaQueryFactory;
+
+	@Override
+	public Optional<Long> findExistingChatRoomIdOnProduct(Long productId, Long sellerId, Long buyerId) {
+
+		Long chatRoomId = jpaQueryFactory
+				.select(chatRoom.id)
+				.from(chatRoom)
+				.join(chatParticipant.chatRoom, chatRoom)
+				.where(chatRoom.product.id.eq(productId)
+						.and(chatParticipant.member.id.in(sellerId, buyerId)))
+				.groupBy(chatRoom.id)
+				.having(chatParticipant.member.id.countDistinct().eq(2L))
+				.fetchFirst();
+
+		return Optional.ofNullable(chatRoomId);
+	}
+}
