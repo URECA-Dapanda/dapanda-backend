53cb88c205fd3900893f4cd426b162e48addf65d
yereumi <lovelyyeji01@gmail.com>
2025-07-20 12:48:27 +0900

    [DP-101] test: 데이터 일반 구매 상품 생성 메서드명 수정 및 TradeType 추가 #84

--- src/test/java/com/dapanda/trade/entity/TradeFixture.java
+++ src/test/java/com/dapanda/trade/entity/TradeFixture.java
@@ -5,19 +5,20 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 public class TradeFixture {
 
-	public static Trade createTrade1(Member member) {
+	public static Trade createTradeMobileDataDefault(Member member) {
 
 		return Trade.of(
 				0.5F,
 				null,
 				1000,
+				TradeType.PURCHASE_SINGLE,
 				member
 		);
 	}
 
 	public static Trade createTrade1WithId(Member member, Long tradeId) {
 
-		Trade trade = createTrade1(member);
+		Trade trade = createTradeMobileDataDefault(member);
 
 		ReflectionTestUtils.setField(trade, "id", tradeId);
 
