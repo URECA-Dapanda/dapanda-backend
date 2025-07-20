3fe8fbb4187d883ac0d83691d04f072c37ff09b8
yereumi <lovelyyeji01@gmail.com>
2025-07-20 20:14:31 +0900

    [DP-102] feat: 데이터 상품 자투리 구매 요청 DTO 추가 #85

---
+++ src/main/java/com/dapanda/trade/dto/request/TradeMobileDataScrapRequest.java
@@ -0,0 +1,18 @@
+package com.dapanda.trade.dto.request;
+
+import com.dapanda.trade.dto.MobileDataScrap;
+import jakarta.validation.constraints.NotNull;
+import java.util.List;
+
+public record TradeMobileDataScrapRequest(
+
+		@NotNull(message = "총 데이터양은 필수입니다.")
+		float totalAmount,
+
+		@NotNull(message = "총 가격은 필수입니다.")
+		int totalPrice,
+
+		@NotNull(message = "데이터 상품 조합 목록은 필수입니다.")
+		List<MobileDataScrap> combinations) {
+
+}
