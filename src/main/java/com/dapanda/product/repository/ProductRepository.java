78eab0a47a70c3d29b66be0f6ac3f2ebe0a89ec9
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-20 18:51:53 +0900

    [DP-79] feat: 상품 아이디 조회 쿼리 추가 #68

--- src/main/java/com/dapanda/product/repository/ProductRepository.java
+++ src/main/java/com/dapanda/product/repository/ProductRepository.java
@@ -2,12 +2,13 @@ package com.dapanda.product.repository;
 
 import com.dapanda.product.entity.Product;
 import jakarta.persistence.LockModeType;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductCustomRepository {
@@ -16,4 +17,6 @@ public interface ProductRepository extends JpaRepository<Product, Long>, Product
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("SELECT p FROM Product p WHERE p.id = :id")
 	Optional<Product> findByIdForUpdate(@Param("id") Long id);
+
+	boolean existsByIdAndMember_Id(Long id, Long memberId);
 }
