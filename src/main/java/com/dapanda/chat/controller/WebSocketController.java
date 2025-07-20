13172c1bed8399243283f416f223bcc87e017c9f
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-20 23:26:52 +0900

    [DP-98] feat: 요청 DTO에 @Valid 추가 #82

--- src/main/java/com/dapanda/chat/controller/WebSocketController.java
+++ src/main/java/com/dapanda/chat/controller/WebSocketController.java
@@ -3,6 +3,7 @@ package com.dapanda.chat.controller;
 import com.dapanda.chat.config.WebSocketPath;
 import com.dapanda.chat.dto.request.ChatMessageRequest;
 import com.dapanda.chat.service.ChatService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -19,7 +20,9 @@ public class WebSocketController {
 	private final ChatService chatService;
 
 	@MessageMapping("/{chatRoomId}")
-	public void sendMessage(@DestinationVariable Long chatRoomId, ChatMessageRequest request) {
+	public void sendMessage(
+			@DestinationVariable Long chatRoomId,
+			@Valid ChatMessageRequest request) {
 
 		log.info("Message : {}", request.message());
 
