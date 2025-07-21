36a20f9e32ea30732c7b3c66deba211143b1b22a
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-21 14:10:34 +0900

    [DP-107] test: 참여중인 채팅방 조회 Controller 테스트 추가 #94

--- src/test/java/com/dapanda/chat/entity/ChatParticipantFixture.java
+++ src/test/java/com/dapanda/chat/entity/ChatParticipantFixture.java
@@ -3,6 +3,9 @@ package com.dapanda.chat.entity;
 import com.dapanda.member.entity.Member;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatParticipantFixture {
 
 	public static ChatParticipant createChatParticipantWithId(ChatRoom chatroom, Member member, Long chatParticipantId) {
@@ -12,5 +15,18 @@ public class ChatParticipantFixture {
 		ReflectionTestUtils.setField(chatParticipant, "id", chatParticipantId);
 
 		return chatParticipant;
+	}
+
+	public static List<ChatParticipant> createChatParticipantList(List<ChatRoom> chatRoomList, Member buyer, Member seller){
+
+		List<ChatParticipant> chatParticipantList = new ArrayList<>();
+
+		for (ChatRoom chatRoom : chatRoomList) {
+
+			chatParticipantList.add(ChatParticipant.of(chatRoom, buyer));
+			chatParticipantList.add(ChatParticipant.of(chatRoom, seller));
+		}
+
+		return chatParticipantList;
 	}
 }
--- src/test/java/com/dapanda/chat/entity/ChatRoomFixture.java
+++ src/test/java/com/dapanda/chat/entity/ChatRoomFixture.java
@@ -3,6 +3,9 @@ package com.dapanda.chat.entity;
 import com.dapanda.product.entity.Product;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatRoomFixture {
 
 	public static ChatRoom createChatRoomWithId(Product product, Long chatRoomId) {
@@ -12,5 +15,17 @@ public class ChatRoomFixture {
 		ReflectionTestUtils.setField(chatRoom, "id", chatRoomId);
 
 		return chatRoom;
+	}
+
+	public static List<ChatRoom> createChatRoomList(List<Product> productList){
+
+		List<ChatRoom> chatRoomList = new ArrayList<>();
+
+		for (Product product : productList) {
+
+			chatRoomList.add(ChatRoom.of(product));
+		}
+
+		return chatRoomList;
 	}
 }
