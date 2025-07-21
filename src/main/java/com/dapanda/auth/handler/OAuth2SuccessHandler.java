43118ce0ae98a6ec2b953d1658c46079b20aea83
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-21 17:03:36 +0900

    [DP-119] feat: OAuth2SuccessHandler에 트랜잭션 추가 #110

--- src/main/java/com/dapanda/auth/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/dapanda/auth/handler/OAuth2SuccessHandler.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 
@@ -30,6 +31,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 	private final PlanService planService;
 
 	@Override
+	@Transactional
 	public void onAuthenticationSuccess(HttpServletRequest request,
 			HttpServletResponse response,
 			Authentication authentication) throws IOException {
