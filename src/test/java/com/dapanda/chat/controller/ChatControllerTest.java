ca4b197593e944c721abd4c62933af1171355b0c
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-21 14:22:12 +0900

    [DP-107] test: 참여중인 채팅방 빈 리스트 반환 테스트 코드 추가 #94

--- src/test/java/com/dapanda/chat/controller/ChatControllerTest.java
+++ src/test/java/com/dapanda/chat/controller/ChatControllerTest.java
@@ -43,6 +43,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWit
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -191,7 +192,7 @@ class ChatControllerTest {
 
 			@Test
 			@DisplayName("페이징 조건이 없으면 기본 값으로 조회된다")
-			public void readChatRoomDefaultPaging() throws Exception {
+			public void readChatRoomDefaultPagingTest() throws Exception {
 
 				//given
 				Member seller = memberRepository.save(MemberFixture.createMember1());
@@ -252,12 +253,50 @@ class ChatControllerTest {
 								)
 						));
 			}
-		}
 
-		@Nested
-		@DisplayName("실패 케이스")
-		class Fail {
+			@Test
+			@DisplayName("조회 결과가 없으면 빈 리스트를 반환한다")
+			public void readChatRoomEmptyListTest() throws Exception {
 
+				//given
+				Member buyer = memberRepository.save(MemberFixture.createMember2());
+
+				CustomUserDetails userDetails = CustomUserDetails.from(buyer);
+
+				//when & then
+				mockMvc.perform(get("/api/chat-room")
+								.with(authentication(new UsernamePasswordAuthenticationToken(
+										userDetails, null, userDetails.getAuthorities()
+								))))
+						.andDo(print())
+						.andExpect(status().isOk())
+						.andExpect(jsonPath("$.code").value(ResultCode.SUCCESS.getCode()))
+						.andExpect(jsonPath("$.message").value(ResultCode.SUCCESS.getMessage()))
+						.andExpect(jsonPath("$.data.data").exists())
+						.andExpect(jsonPath("$.data.data.length()").value(0))
+						.andExpect(jsonPath("$.data.pageInfo").exists())
+						.andExpect(jsonPath("$.data.pageInfo.hasNext").value(false))
+						.andExpect(jsonPath("$.data.pageInfo.size").value(DEFAULT_SIZE))
+						.andExpect(jsonPath("$.data.pageInfo.nextCursorId").doesNotExist())
+						.andDo(document("chat/read-chat-room-empty-list",
+								queryParameters(
+										parameterWithName("cursorId").description("커서 아이디 (선택)").optional(),
+										parameterWithName("size").description("페이지 크기 (선택, 기본값 = 2, 최대 = 100)").optional(),
+										parameterWithName("chatRoomReadOption").description("채팅방 조회 옵션 (선택, 기본값 = ALL: 전체, BUYER: 구매자 기준, SELLER: 판매자 기준)").optional()
+								),
+								responseFields(
+										fieldWithPath("code").description("응답 코드"),
+										fieldWithPath("message").description("응답 메시지"),
+										fieldWithPath("data").description("페이징 처리된 리뷰 데이터"),
+										// data.data[] 배열 내의 각 리뷰 객체 필드
+										fieldWithPath("data.data[]").description("조회된 채팅방 목록"),
+										fieldWithPath("data.pageInfo").description("페이지 정보"),
+										fieldWithPath("data.pageInfo.size").description("현재 페이지 크기"),
+										fieldWithPath("data.pageInfo.hasNext").description("다음 페이지 존재 여부"),
+										fieldWithPath("data.pageInfo.nextCursorId").description("다음 페이지 조회 시 사용할 커서 아이디 (다음 페이지가 없으면 null)")
+								)
+						));
+			}
 		}
 	}
 }
