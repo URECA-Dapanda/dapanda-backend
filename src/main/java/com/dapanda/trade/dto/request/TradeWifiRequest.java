9bab9ad831e4523fabc418d5df11ec88de3bde34
yereumi <lovelyyeji01@gmail.com>
2025-07-21 10:08:31 +0900

    [DP-103] feat: 와이파이 구매 요청 DTO 추가 #86

---
+++ src/main/java/com/dapanda/trade/dto/request/TradeWifiRequest.java
@@ -0,0 +1,20 @@
+package com.dapanda.trade.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+public record TradeWifiRequest(
+
+		@NotNull(message = "상품 아이디는 필수입니다.")
+		Long productId,
+
+		@NotNull(message = "와이파이 아이디는 필수입니다.")
+		Long wifiId,
+
+		@NotNull(message = "시작 시간은 필수입니다.")
+		LocalDateTime startTime,
+
+		@NotNull(message = "종료 시간은 필수입니다.")
+		LocalDateTime endTime) {
+
+}
