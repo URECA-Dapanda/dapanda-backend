4ce0f527a6974676b0df304e0978477469c800bf
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-07-21 13:22:07 +0900

    [DP-114] feat : Member 엔티티에 받은 리뷰 갯수와 현재 평균 별점 추가 #102

--- src/main/java/com/dapanda/member/entity/Member.java
+++ src/main/java/com/dapanda/member/entity/Member.java
@@ -60,6 +60,10 @@ public class Member extends BaseEntity {
 
 	private int reportedCount;
 
+	private int reviewCount = 0;
+
+	private double averageRating = 0.0;
+
 	public static Member ofOAuthMember(String email, String name,
 			OAuthProvider provider, MemberRole role) {
 
@@ -108,5 +112,10 @@ public class Member extends BaseEntity {
 	public void addSellingData(float sellingData) {
 
 		this.sellingData += sellingData;
+	}
+
+	public void updateReviewInfo(int reviewCount, double averageRating) {
+		this.reviewCount = reviewCount;
+		this.averageRating = averageRating;
 	}
 }
