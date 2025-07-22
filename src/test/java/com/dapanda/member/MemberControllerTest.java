dfa0f28d1adce5f4d1c0d61743e8774deadedaf8
yereumi <lovelyyeji01@gmail.com>
2025-07-22 15:12:16 +0900

    [DP-128] test: 보유 캐시 조회 로직 통합 테스트 작성 #121

---
+++ src/test/java/com/dapanda/member/MemberControllerTest.java
@@ -0,0 +1,121 @@
+package com.dapanda.member;
+
+import static com.dapanda.TestConstants.Member.CASH_5000;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.dapanda.TestConfig;
+import com.dapanda.auth.entity.CustomUserDetails;
+import com.dapanda.common.exception.ResultCode;
+import com.dapanda.member.entity.Member;
+import com.dapanda.member.entity.MemberFixture;
+import com.dapanda.member.repository.MemberRepository;
+import com.dapanda.member.service.MemberService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@Import(TestConfig.class)
+@ActiveProfiles("test")
+@ExtendWith(RestDocumentationExtension.class)
+@DisplayName("회원 컨트롤러 테스트")
+class MemberControllerTest {
+
+	@Autowired
+	private WebApplicationContext context;
+	@Autowired
+	private ObjectMapper objectMapper;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private EntityManager entityManager;
+	@Autowired
+	private MemberRepository memberRepository;
+	@Autowired
+	private MemberService memberService;
+
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	void restDocsSetUp(RestDocumentationContextProvider restDocumentation) {
+
+		this.mockMvc = TestConfig.createMockMvc(context, restDocumentation);
+
+		cleanupDatabase();
+	}
+
+	private void cleanupDatabase() {
+
+		entityManager.clear();
+
+		jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 0");
+
+		jdbcTemplate.execute("TRUNCATE TABLE member");
+
+		jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 1");
+	}
+
+	@Nested
+	@DisplayName("캐시 조회 API")
+	class MobileDataFullDefaultPurchase {
+
+		@Nested
+		@DisplayName("성공 케이스")
+		class Success {
+
+			@Test
+			@DisplayName("데이터 통합 상품 일반 구매를 성공한다")
+			void findCash() throws Exception {
+
+				// given
+				Member member = MemberFixture.createMember1();
+				ReflectionTestUtils.setField(member, "cash", CASH_5000);
+				memberRepository.save(member);
+
+				CustomUserDetails userDetails = CustomUserDetails.from(member);
+
+				// when & then
+				mockMvc.perform(get("/api/members/cash")
+								.contentType(MediaType.APPLICATION_JSON)
+								.with(authentication(new UsernamePasswordAuthenticationToken(
+										userDetails, null, userDetails.getAuthorities()
+								)))
+						)
+						.andExpect(status().isOk())
+						.andExpect(jsonPath("$.code").value(ResultCode.SUCCESS.getCode()))
+						.andExpect(jsonPath("$.message").value(ResultCode.SUCCESS.getMessage()))
+						.andExpect(jsonPath("$.data.cash").value(CASH_5000))
+						.andDo(document("member/get-cash",
+								responseFields(
+										fieldWithPath("code").description("상태 코드"),
+										fieldWithPath("message").description("처리 결과 메시지"),
+										fieldWithPath("data").description("응답 데이터 (에러시 반환되지 않음)"),
+										fieldWithPath("data.cash").description("회원이 보유한 캐시")
+								))
+						);
+			}
+		}
+	}
+}
