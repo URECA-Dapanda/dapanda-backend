a14ba542cc29c18defc1602a7b4478499da646e3
yereumi <lovelyyeji01@gmail.com>
2025-07-22 15:09:03 +0900

    [DP-128] chore: 불필요한 공백 삭제 #121

--- src/test/java/com/dapanda/product/service/ProductServiceTest.java
+++ src/test/java/com/dapanda/product/service/ProductServiceTest.java
@@ -83,7 +83,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-
 @ExtendWith(MockitoExtension.class)
 @DisplayName("상품 서비스 테스트")
 class ProductServiceTest {
