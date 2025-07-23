4e040c786e7282f8430825bb7a723daa3e936369
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-23 10:30:17 +0900

    [DP-115] feat: 채팅 이력 조회 페이징 쿼리 추가 #105

---
+++ src/main/java/com/dapanda/chat/repository/ChatMessageRepositoryImpl.java
@@ -0,0 +1,45 @@
+package com.dapanda.chat.repository;
+
+import com.dapanda.chat.dto.request.ReadChatMessageHistoryRequest;
+import com.dapanda.chat.dto.response.SendChatMessageResponse;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static com.dapanda.chat.entity.QChatMessage.chatMessage;
+import static com.dapanda.chat.entity.QChatRoom.chatRoom;
+
+@RequiredArgsConstructor
+public class ChatMessageRepositoryImpl implements ChatMessageRepositoryCustom {
+
+	private final JPAQueryFactory jpaQueryFactory;
+
+	@Override
+	public List<SendChatMessageResponse> findChatMessageHistory(ReadChatMessageHistoryRequest request) {
+
+		BooleanBuilder whereClause = new BooleanBuilder();
+
+		whereClause.and(chatRoom.id.eq(request.chatRoomId()));
+
+		if (request.cursorId() != null && !request.cursorId().equals(0L)) {
+			whereClause.and(chatMessage.id.lt(request.cursorId()));
+		}
+
+		return jpaQueryFactory
+				.select(Projections.constructor(SendChatMessageResponse.class,
+						chatMessage.id,
+						chatMessage.member.id,
+						chatMessage.message,
+						chatMessage.createdAt
+				))
+				.from(chatMessage)
+				.join(chatMessage.chatRoom, chatRoom)
+				.where(whereClause)
+				.orderBy(chatMessage.id.asc())
+				.limit(request.size() + 1)
+				.fetch();
+	}
+}
