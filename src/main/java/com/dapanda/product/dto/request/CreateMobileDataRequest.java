b0bb96323ecbcd2c4e009597d9157bacfb5aff43
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-07-23 23:41:33 +0900

    [DP-155] feat : 데이터 상품 생성 DTO에서 @Decimal으로 값 검증 #153

--- src/main/java/com/dapanda/product/dto/request/CreateMobileDataRequest.java
+++ src/main/java/com/dapanda/product/dto/request/CreateMobileDataRequest.java
@@ -1,12 +1,14 @@
 package com.dapanda.product.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 @Getter
 public class CreateMobileDataRequest extends CreateProductRequest {
 
 	@NotNull(message = "dataAmount는 null일 수 없습니다.")
+	@DecimalMin(value = "0.1", message = "dataAmount는 최소 0.1GB 이상이어야 합니다.")
+	@DecimalMax(value = "2.0", message = "dataAmount는 최대 2.0GB 이하여야 합니다.")
 	private final Float dataAmount; // GB 단위
 
 	@NotNull(message = "isSplitType는 null일 수 없습니다.")
