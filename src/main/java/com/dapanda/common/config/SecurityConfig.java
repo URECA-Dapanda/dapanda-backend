972b326966aac93525c159f483a15ae3dceed215
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-23 13:07:04 +0900

    [DP-115] feat: WebSocket 연결 경로 변경 #105

--- src/main/java/com/dapanda/common/config/SecurityConfig.java
+++ src/main/java/com/dapanda/common/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/", "/api/**", "/api-docs.html", "/docs/**",
 								"/oauth2/**", "error", "/actuator/health", "/default-ui.css",
-								"/api/auth/**", "/connect/**").permitAll()
+								"/api/auth/**", "/conn/**").permitAll()
 						.anyRequest().authenticated()
 				)
 				.oauth2Login(oauth2 -> oauth2
