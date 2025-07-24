b0a2a867329e0e1501b113aa383a870c76134e9e
yereumi <lovelyyeji01@gmail.com>
2025-07-24 15:06:15 +0900

    [DP-163] refactor: 상품 아이디 productId로 수정 #129

--- src/main/java/com/dapanda/product/dto/ProductSummary.java
+++ src/main/java/com/dapanda/product/dto/ProductSummary.java
@@ -1,15 +1,13 @@
 package com.dapanda.product.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductSummary {
 
-	private Long id;
+	private Long productId;
 	private int price;
 	private Long itemId;
 	private String memberName;
