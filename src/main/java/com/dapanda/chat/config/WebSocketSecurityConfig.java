110879cb8f745fd7c77609bbf19f832903db2238
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-24 13:34:09 +0900

    [DP-115] feat: @EnableWebSocketSecurity 를 이용한 인증 객체 바인딩 처리 추가  #105

---
+++ src/main/java/com/dapanda/chat/config/WebSocketSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.dapanda.chat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+
+@Configuration
+@EnableWebSocketSecurity
+public class WebSocketSecurityConfig {
+
+	@Bean
+	AuthorizationManager<Message<?>> messageAuthorizationManager(MessageMatcherDelegatingAuthorizationManager.Builder messages) {
+
+		return messages
+				.anyMessage().authenticated()
+				.build();
+	}
+
+	@Bean("csrfChannelInterceptor") // for disable csrf
+	public ChannelInterceptor csrfChannelInterceptor() {
+		return new ChannelInterceptor() {
+		};
+	}
+}
