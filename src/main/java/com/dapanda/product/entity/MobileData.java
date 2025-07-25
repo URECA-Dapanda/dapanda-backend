74c33c062ce44f4df0463b831a489f9734ae95e4
yereumi <lovelyyeji01@gmail.com>
2025-07-25 20:48:46 +0900

    [DP-179] fix: 데이터 상품 수정시 dataAmount로 데이터양 적용 #178

--- src/main/java/com/dapanda/product/entity/MobileData.java
+++ src/main/java/com/dapanda/product/entity/MobileData.java
@@ -50,7 +50,7 @@ public class MobileData {
 	public void updateMobileData(BigDecimal changedAmount, int pricePer100MB, boolean isSplitType) {
 
 		this.dataAmount = this.dataAmount.add(changedAmount);
-		this.remainAmount = this.remainAmount.add(changedAmount);
+		this.remainAmount = changedAmount;
 		this.pricePer100MB = pricePer100MB;
 		this.isSplitType = isSplitType;
 	}
