20175ae0d90f07e2db462cba46e7d7ae7a283f6e
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-27 23:13:17 +0900

    [DP-189] feat: 채팅방 목록 조회 응답 DTO에 마지막 메시지 필드 추가 #196

--- src/main/java/com/dapanda/chat/dto/response/ReadJoiningChatRoomResponse.java
+++ src/main/java/com/dapanda/chat/dto/response/ReadJoiningChatRoomResponse.java
@@ -15,6 +15,7 @@ public class ReadJoiningChatRoomResponse {
 	private Long chatRoomId;
 	private LocalDateTime createdAt;
 	private LocalDateTime lastMessageAt;
+	private String lastMessage;
 
 	//Member
 	private Long senderId;
@@ -33,6 +34,7 @@ public class ReadJoiningChatRoomResponse {
 			Long chatRoomId,
 			LocalDateTime createdAt,
 			LocalDateTime lastMessageAt,
+			String lastMessage,
 			Long senderId,
 			String senderName,
 			Long productId,
@@ -45,6 +47,7 @@ public class ReadJoiningChatRoomResponse {
 				.chatRoomId(chatRoomId)
 				.createdAt(createdAt)
 				.lastMessageAt(lastMessageAt)
+				.lastMessage(lastMessage)
 				.senderId(senderId)
 				.senderName(senderName)
 				.productId(productId)
