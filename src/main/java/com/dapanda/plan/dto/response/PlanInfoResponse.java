d5418e149c407b195cad343cc9e3387ed20cc57b
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-07-28 02:25:18 +0900

    [DP-172] feat : 요금제 조회 응답 DTO에서도 Plan 생성 시 데이터 총량 추가 #175

--- src/main/java/com/dapanda/plan/dto/response/PlanInfoResponse.java
+++ src/main/java/com/dapanda/plan/dto/response/PlanInfoResponse.java
@@ -11,6 +11,7 @@ import lombok.*;
 public class PlanInfoResponse {
 
 	private String name;
+	private BigDecimal availableDataAmount;
 	private BigDecimal providingDataAmount;
 	private int monthlyPrice;
 
@@ -18,7 +19,8 @@ public class PlanInfoResponse {
 
 		return PlanInfoResponse.builder().
 				name(plan.getName()).
-				providingDataAmount(plan.getProvidingDataAmount()).
+				availableDataAmount(plan.getAvailableDataAmount()).
+				providingDataAmount(plan.getAvailableDataAmount()).
 				monthlyPrice(plan.getMonthlyPrice()).
 				build();
 	}
