9e21fb7d43705e30e7b99dc1461e02a302298e9a
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-30 14:12:41 +0900

    [DP-220] feat: 사용하지 않는 메서드 제거 #228

--- src/main/java/com/dapanda/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/dapanda/chat/repository/ChatMessageRepository.java
@@ -4,6 +4,4 @@ import com.dapanda.chat.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long>, ChatMessageRepositoryCustom {
-
-	boolean existsByIdAndMember_IdAndChatRoom_Id(Long id, Long memberId, Long chatRoomId);
 }
