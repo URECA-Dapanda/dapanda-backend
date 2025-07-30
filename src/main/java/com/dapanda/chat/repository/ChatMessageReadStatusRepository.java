d7984ee0ede7e4cec20dfa43e0e77595c49c2b5c
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-30 14:28:55 +0900

    [DP-220] test: 테스트에서만 사용하는 메서드 제거 #228

--- src/main/java/com/dapanda/chat/repository/ChatMessageReadStatusRepository.java
+++ src/main/java/com/dapanda/chat/repository/ChatMessageReadStatusRepository.java
@@ -1,14 +1,13 @@
 package com.dapanda.chat.repository;
 
-import com.dapanda.chat.entity.*;
+import com.dapanda.chat.entity.ChatMessageReadStatus;
+import com.dapanda.chat.entity.ChatRoom;
 import com.dapanda.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ChatMessageReadStatusRepository extends JpaRepository<ChatMessageReadStatus, Long> {
-
-	Optional<ChatMessageReadStatus> findFirstByChatRoomAndChatMessageAndMember(ChatRoom chatRoom, ChatMessage chatMessage, Member member);
 
 	Optional<ChatMessageReadStatus> findFirstByChatRoomAndMember(ChatRoom chatRoom, Member member);
 }
