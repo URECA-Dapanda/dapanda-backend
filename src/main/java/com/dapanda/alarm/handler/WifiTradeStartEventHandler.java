1b5888e35d5384a6d7bc988d76dca5c813861056
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-31 19:22:50 +0900

    [DP-242] feat: 웹소켓 기반 알림 발행 기능 추가 #246

---
+++ src/main/java/com/dapanda/alarm/handler/WifiTradeStartEventHandler.java
@@ -0,0 +1,30 @@
+package com.dapanda.alarm.handler;
+
+import com.dapanda.alarm.event.WifiTradeStartEvent;
+import com.dapanda.chat.config.WebSocketPath;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WifiTradeStartEventHandler {
+
+	private final SimpMessagingTemplate messagingTemplate;
+
+	@Async
+	@EventListener
+	public void handleWifiTradeStart(WifiTradeStartEvent event) {
+
+		log.info("WIFI 사용 시작 이벤트 tradeId : {}, memberId : {}", event.getTradeId(), event.getMemberId());
+
+		messagingTemplate.convertAndSend(
+				WebSocketPath.SUB.getPath() + "/" + WebSocketPath.ALARM.getPath() + event.getMemberId(),
+				event
+		);
+	}
+}
