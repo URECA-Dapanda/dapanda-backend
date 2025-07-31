d74389ee827a27b00419d3e8dd63592c192eac4d
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-07-31 18:03:20 +0900

    [DP-328] feat: Redis PubSub 구조 활용 코드로 변경 #245

--- src/main/java/com/dapanda/chat/controller/WebSocketController.java
+++ src/main/java/com/dapanda/chat/controller/WebSocketController.java
@@ -1,15 +1,16 @@
 package com.dapanda.chat.controller;
 
 import com.dapanda.auth.entity.CustomUserDetails;
-import com.dapanda.chat.config.WebSocketPath;
 import com.dapanda.chat.dto.request.CreateChatMessageRequest;
 import com.dapanda.chat.dto.response.CreateMessageResponse;
 import com.dapanda.chat.service.ChatService;
+import com.dapanda.chat.service.RedisPubSubService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.*;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
@@ -18,20 +19,18 @@ import org.springframework.stereotype.Controller;
 @RequiredArgsConstructor
 public class WebSocketController {
 
-	private final SimpMessagingTemplate messageTemplate;
+	private final RedisPubSubService redisPubSubService;
+	private final ObjectMapper objectMapper;
 	private final ChatService chatService;
 
 	@MessageMapping("/{chatRoomId}")
 	public void sendMessage(
 			@DestinationVariable Long chatRoomId,
 			@Payload @Valid CreateChatMessageRequest request,
-			@AuthenticationPrincipal CustomUserDetails userDetails) {
+			@AuthenticationPrincipal CustomUserDetails userDetails) throws JsonProcessingException {
 
 		CreateMessageResponse response = chatService.createChatMessage(chatRoomId, request, userDetails.getId());
 
-		messageTemplate.convertAndSend(
-				WebSocketPath.SUB.getPath() + WebSocketPath.SLASH.getPath() + chatRoomId,
-				response
-		);
+		redisPubSubService.publish("chat", objectMapper.writeValueAsString(response));
 	}
 }
