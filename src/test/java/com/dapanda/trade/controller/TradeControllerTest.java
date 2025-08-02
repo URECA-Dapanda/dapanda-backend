319b597b68e4c18f4f946929f92101460857e1a7
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-08-02 17:46:29 +0900

    [DP-255] test : 리뷰 등록시 fcm 토큰이 없는 오류 수정 #260

--- src/test/java/com/dapanda/trade/controller/TradeControllerTest.java
+++ src/test/java/com/dapanda/trade/controller/TradeControllerTest.java
@@ -1013,6 +1013,8 @@ class TradeControllerTest {
 				ReflectionTestUtils.setField(buyer, "cash", CASH_5000);
 				List<Member> members = new ArrayList<>(Arrays.asList(seller, buyer));
 				memberRepository.saveAll(members);
+				fcmTokenRepository.save(FcmToken.of("test_token1", seller));
+				fcmTokenRepository.save(FcmToken.of("test_token2", buyer));
 
 				Wifi wifi = WifiFixture.createWifi(TITLE, CONTENT, LATITUDE, LONGITUDE, ADDRESS,
 						START_DATETIME, END_DATETIME);
@@ -1034,6 +1036,7 @@ class TradeControllerTest {
 										userDetails, null, userDetails.getAuthorities()
 								)))
 						)
+						.andDo(print())
 						.andExpect(status().isOk())
 						.andExpect(jsonPath("$.code").value(ResultCode.SUCCESS.getCode()))
 						.andExpect(jsonPath("$.message").value(ResultCode.SUCCESS.getMessage()))
