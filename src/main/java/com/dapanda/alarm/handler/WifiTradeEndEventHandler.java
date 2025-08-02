e1bca15a08b2dbbdc209e88ea4de6eaca30b3368
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-08-03 01:14:57 +0900

    [DP-255] feat : 와이파이 종료 이벤트 핸들러 추가 #260

---
+++ src/main/java/com/dapanda/alarm/handler/WifiTradeEndEventHandler.java
@@ -0,0 +1,35 @@
+package com.dapanda.alarm.handler;
+
+import com.dapanda.alarm.event.WifiTradeEndEvent;
+import com.dapanda.chat.config.WebSocketPath;
+import com.dapanda.fcmToken.service.FcmTokenService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WifiTradeEndEventHandler {
+
+	private final SimpMessagingTemplate messagingTemplate;
+	private final FcmTokenService fcmTokenService;
+
+	@Async
+	@EventListener
+	public void handleWifiTradeEnd(WifiTradeEndEvent event) {
+		log.info("WIFI 사용 종료 이벤트 tradeId : {}, memberId : {}", event.getTradeId(),
+				event.getMemberId());
+
+		messagingTemplate.convertAndSend(
+				WebSocketPath.SUB.getPath() + "/" + WebSocketPath.ALARM.getPath()
+						+ event.getMemberId(),
+				event
+		);
+
+		fcmTokenService.notifyWifiEnd(event.getMemberId());
+	}
+}
