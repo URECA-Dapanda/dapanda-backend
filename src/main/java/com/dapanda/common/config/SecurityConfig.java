329e3ab75863b4fb1df18c391700186c4ceb2553
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-08-06 16:18:35 +0900

    [DP-331] refactor: 사용하지 않는 Bean 제거 #329

--- src/main/java/com/dapanda/common/config/SecurityConfig.java
+++ src/main/java/com/dapanda/common/config/SecurityConfig.java
@@ -4,20 +4,13 @@ import com.dapanda.auth.handler.OAuth2FailureHandler;
 import com.dapanda.auth.handler.OAuth2SuccessHandler;
 import com.dapanda.auth.service.CustomOAuth2UserService;
 import com.dapanda.jwt.JwtAuthenticationFilter;
-import com.dapanda.jwt.JwtTokenProvider;
-import com.dapanda.member.service.MemberService;
-import com.dapanda.refreshToken.service.RefreshTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.*;
@@ -29,19 +22,10 @@ public class SecurityConfig {
 	private final CustomOAuth2UserService customOAuth2UserService;
 	private final OAuth2SuccessHandler oAuth2SuccessHandler;
 	private final OAuth2FailureHandler oAuth2FailureHandler;
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
 	@Bean
-	public JwtAuthenticationFilter jwtAuthenticationFilter(
-			JwtTokenProvider jwtTokenProvider,
-			MemberService memberService,
-			RefreshTokenService refreshTokenService
-	) {
-		return new JwtAuthenticationFilter(jwtTokenProvider, memberService, refreshTokenService);
-	}
-
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http,
-			JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 		http
 				.cors(cors -> cors.configurationSource(corsConfigurationSource()))
@@ -74,19 +58,6 @@ public class SecurityConfig {
 		);
 
 		return http.build();
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
-			throws Exception {
-
-		return configuration.getAuthenticationManager();
 	}
 
 	@Bean
