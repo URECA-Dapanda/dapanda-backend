8a80ec3fa242f43e7142813dd028adab27bf3521
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-08-06 16:19:34 +0900

    [DP-331] refactor: 컴포넌트 스캔을 위한 컴포넌트 등록 #329

--- src/main/java/com/dapanda/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/dapanda/jwt/JwtAuthenticationFilter.java
@@ -15,12 +15,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.nio.file.AccessDeniedException;
 
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
